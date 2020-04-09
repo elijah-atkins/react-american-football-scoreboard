fd1fa05f189003a4d423a8a16a4b27f39f3e359a
elijah-atkins <elijahorama@gmail.com>
2020-04-08 23:19:16 -0600

    added countdown timer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import BottomRow, { nextQuarter } from "./BottomRow";
+import BottomRow from "./BottomRow";
+function secondsToTime(secs){
+  let hours = Math.floor(secs / (60 * 60));
 
+  let divisor_for_minutes = secs % (60 * 60);
+  let minutes = Math.floor(divisor_for_minutes / 60);
+
+  let divisor_for_seconds = divisor_for_minutes % 60;
+  let seconds = Math.ceil(divisor_for_seconds);
+
+  let obj = {
+    "h": hours,
+    "m": minutes,
+    "s": seconds
+  };
+  return obj;
+}
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHome] = useState(0);
   const [awayScore, setAway] = useState(0);
+  const [counter, setCounter] = useState(4500)
+  const [pause, setPause] = useState(false)
   useEffect(()=>{
-    
-  })
+    if (!pause){
+      const timer =
+        counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+    return () => clearInterval(timer);
+    }
+  }, [counter]);
   return (
     <div className="container">
       <section className="scoreboard">
@@ -21,7 +42,7 @@ function App() {
 
   <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+  <div className="timer">{secondsToTime(counter).m}:{secondsToTime(counter).s}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
   <div className="away__score">{awayScore}</div>
@@ -39,7 +60,11 @@ function App() {
           <button className="awayButtons__touchdown" onClick={()=> setAway(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={()=> setAway(awayScore + 3)}>Away Field Goal</button>
         </div>
-
+        <div className="timerButtons">
+          <button className="resetTimerButton" onClick={()=>{
+            setCounter(4500)
+          }}>Reset Timer</button>
+        </div>
       </section>
     </div>
   );
